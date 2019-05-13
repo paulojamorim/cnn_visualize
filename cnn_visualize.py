657a0bbddb8d56cd1140ef437ba97aee8c6b36ec
Paulo Henrique Junqueira Amorim <paulojamorim@gmail.com>
2019-05-13 15:29:04 -0300

    First commit with a skeatch to visualize CNN filters from Keras models

---
+++ cnn_visualize.py
@@ -0,0 +1,99 @@
+#author: Paulo Henrique Junqueira Amorim
+
+#based on:
+#https://machinelearningmastery.com/how-to-visualize-filters-and-feature-maps-in-convolutional-neural-networks/
+
+import os
+import sys
+import imageio
+from keras.applications.vgg16 import VGG16
+import html_template as ht
+
+def write_html(files_path, html_path):
+    html_file = open(html_path, 'w+')
+ 
+    layers_html = ''
+    for layer in files_path:
+        
+        if layer != []:
+            
+            filters_html = ''
+            for filters in layer:
+
+                    cell_line = ''
+                    for channel in filters:
+                        cell_line += ht.cell(channel)
+                    
+                    filters_html += ht.row(cell_line)
+                  
+    html = ht.table(filters_html)
+
+    html_file.write(html)
+    
+
+def filter_visualize(model, layer_number, out_dir):
+    """
+    Visualize convolutional filters from layers. 
+    model (a keras model) 
+    layer_number=1 (default first layer), -1 (to visualize all layers)
+    """
+    i_layer = 1
+    layer_ = []
+
+    for layer in model.layers:
+
+        if 'conv' in  layer.name:
+            filters, biases = layer.get_weights()
+
+            fshape = filters.shape
+            n_filters = fshape[-1]
+            depth = fshape[-2]
+            
+            #normalize 0-1
+            f_min, f_max = filters.min(), filters.max()
+            filters = (filters - f_min) / (f_max - f_min)
+           
+            filters_ = []
+            ix = 1
+            for i in range(n_filters):
+                
+                # get the filter
+                f = filters[:, :, :, i]
+                
+                # plot each channel separately
+                channels_ = []
+                for j in range(depth):
+                   
+                    if layer_number != -1:
+
+                        if i_layer == layer_number:
+                            fname = str(i_layer) + '_' + str(ix) + '_' + str(j) + '.png'
+                            fpath = os.path.join(out_dir,fname)
+                            imageio.imsave(fpath, f[:,:,j])
+
+                            channels_.append(fpath)
+                    ix += 1
+
+                if layer_number != -1:
+                    if i_layer == layer_number:
+                        filters_.append(channels_)
+    
+            i_layer += 1
+            layer_.append(filters_)
+    
+    return layer_
+
+if __name__ == '__main__':
+
+    #Change for your model
+    model = VGG16()
+    
+    out_dir = os.path.abspath(sys.argv[1]) + '/img/'
+    
+    if not(os.path.exists(out_dir)):
+        os.makedirs(out_dir)
+    
+    file_list = filter_visualize(model, 1, out_dir)
+    html_path = os.path.join(os.path.abspath(os.path.join(out_dir,"../")),'index.html')
+    
+    write_html(file_list, html_path)
---
+++ html_template.py
@@ -0,0 +1,36 @@
+
+def table(text):
+    #to disable interpolation
+    css = '<style type="text/css">\
+            img { image-rendering: optimizeSpeed;\
+            image-rendering: -moz-crisp-edges;\
+            image-rendering: -o-crisp-edges;\
+            image-rendering: -webkit-optimize-contrast;\
+            image-rendering: pixelated;\
+            image-rendering: optimize-contrast;\
+            -ms-interpolation-mode: nearest-neighbor;} </style>\n'
+
+
+    table_start = '<table style="width:100%">'
+    table_end = '</table>'
+
+    return '<body>' + table_start + '\n' +\
+            css + text + table_end + '\n'\
+            '</body>'
+
+def row(text):
+
+    row_start = '<tr>'
+    row_end = '</tr>'
+
+    return row_start + '\n' + text + row_end + '\n'
+
+def cell(img_path):
+
+    cell_start = '<td>'
+    cell_end = '</td>'
+
+    img = '<img src="' + img_path + '" height="42" width="42">'
+
+    return cell_start + '\n' + img + cell_end + '\n'
+
